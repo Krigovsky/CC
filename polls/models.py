397b5eaac579509c45fec94bc5ee0b763f15754b
Luke <luke@globaltraining.edu.au>
2024-08-20 15:49:51 +1000

    Up to improving views - part 5

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -8,7 +9,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    
+    def was_published_recently (self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+ 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
