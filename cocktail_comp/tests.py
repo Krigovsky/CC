7d4f86c4ab9cfe7b4bf773e844faaed56f23e588
Luke <luke@globaltraining.edu.au>
2025-08-15 13:59:43 +1000

    Testing updates

--- cocktail_comp/tests.py
+++ cocktail_comp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from models import Couple
+
 # Create your tests here.
+class Test_Models (TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(
+            first_name = "John",
+            last_name = "Smith",
+            username = "John_Smith",
+        )
+        self.user2 = User.objects.create(
+            first_name = "Jane",
+            last_name = "Smith",
+            username = "Jane_Smith",
+        )
+        self.couple1 = Couple.objects.create(
+            team = "Team 1",
+            partner_names = f"{}"
+        )
+
+    def test_user (self):
+        self.assertEqual(self.user1.first_name, 'John')
+        self.assertEqual(self.user1.last_name, 'Smith')
+        self.assertEqual(self.user1.username, "John_Smith")
+
+    def test_couple(self):
+
+
